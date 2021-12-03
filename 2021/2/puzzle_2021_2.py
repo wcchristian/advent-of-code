947dab3bf2912c54d90b20d80a9a898a11d85f46
Christian Andersen <c.andersen2012@gmail.com>
2021-12-02 20:09:18 -0600

    Problem 2 Solved (4/4 stars)

--- 2021/2/puzzle_2021_2.py
+++ 2021/2/puzzle_2021_2.py
@@ -1,18 +1,18 @@
 import os
 
-_filename = "example_input.txt"
+_filename = "input.txt"
 
 def main():
     # Load in the file
     command_list = load_data()
-
-    # Initialize the variables
-
+    
     # Go through and find out how the things in the file affect the variables
-    position = process_command_list(command_list)
+    p1_position = p1_process_command_list(command_list)
+    p2_position = p2_process_command_list(command_list)
 
     # Multiply and produce the output
-    print(f"The result of {position} is {position[0] * position[1]}")
+    print(f"Part 1: The result of {p1_position} is {p1_position[0] * p1_position[1]}")
+    print(f"Part 2: The result of {p2_position} is {p2_position[0] * p2_position[1]}")
 
 
 """Process the command list and determine the final position
@@ -21,12 +21,13 @@ def main():
 :return: tuple containing the final horizontal position and depth position (horizontal, depth)
 
 """
-def process_command_list(command_list):
+def p1_process_command_list(command_list):
+    horizontal_position = 0
+    depth = 0
+
     for command in command_list:
         command_direction = command[0]
         command_distance = command[1]
-        horizontal_position = 0
-        depth = 0
 
 
         match command_direction:
@@ -38,10 +39,39 @@ def process_command_list(command_list):
                 depth -= command_distance
             case "down":
                 depth += command_distance
-            case _:
-                break;
 
-    return (2, 3)
+    return (horizontal_position, depth)
+
+
+"""Process the command list considering aim up and down rather than depth
+
+:param command_list: The list of tuple commands to process
+:return: tuple containing the position and depth (horizontal, depth) 
+
+"""
+def p2_process_command_list(command_list):
+    horizontal_position = 0
+    aim = 0
+    depth = 0
+
+    for command in command_list:
+        command_direction = command[0]
+        command_distance = command[1]
+
+
+        match command_direction:
+            case "forward":
+                horizontal_position += command_distance
+                depth += command_distance * aim
+            case "backward":
+                horizontal_position -= command_distance
+                depth -= command_distance * aim
+            case "up":
+                aim -= command_distance
+            case "down":
+                aim += command_distance
+
+    return (horizontal_position, depth)
 
 
 """Load the input file lines and set the command into a list of tuples.
---
+++ 2021/README.md
@@ -0,0 +1,2 @@
+# Info
+Puzzles solved in 2021 were done with python 3.10 unless otherwise noted.
