817f3e47d6be5db5d85b2317b734cc221907108c
wcchristian <c.andersen2012@gmail.com>
2024-12-05 14:15:31 -0600

    day 4: Not my cleanest work in order to catch up

--- 2024/day4/day4.py
+++ 2024/day4/day4.py
@@ -11,15 +11,36 @@ neighbor_direction_tuple_map = {
     "NW": (-1, -1)
 }
 
+diag_neighbor_direction_tuple_map = {
+    "NE": (-1, 1),
+    "SE": (1, 1),
+    "SW": (1, -1),
+    "NW": (-1, -1)
+}
+
+neighbor_compliment = {
+    "NE": "SW",
+    "SE": "NW",
+    "SW": "NE",
+    "NW": "SE"
+}
+
+next_letter_map = {
+    "X": "M",
+    "M": "A",
+    "A": "S",
+    "S": None
+}
+
 def main():
     filename = "day4_input.txt"
     example_filename = "day4_example.txt"
 
     print(f'Part 1 Example: {part1(example_filename)}')
-    # print(f'Part 1: {part1(filename)}')
-    # print(f'Part 2 Example: {part2(example_filename)}')
-    # print(f'Part 2: {part2(filename)}')
-    
+    print(f'Part 1: {part1(filename)}')
+    print(f'Part 2 Example: {part2(example_filename)}')
+    print(f'Part 2: {part2(filename)}')
+
 
 def part1(filename):
     grid = read_input(filename)
@@ -29,49 +50,73 @@ def part1(filename):
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x] == 'X':
-                counter += depth_first_search_for_christmas((y, x), grid) #TODO: Switch around x and y?
-
-    # Process X
+                counter += depth_first_search_for_christmas((y, x), grid)
 
     return counter
 
 
 def part2(filename):
-    lines = read_input(filename)
+    grid = read_input(filename)
 
-    return 0
-
-def depth_first_search_for_christmas(coord, grid):
-
-    # Find all of the neighbors that match (CHECK BOUNDS OF THE ARRAY, IF WE HIT A WALL, WE ARE DONE)
+    # Find A
     counter = 0
-    for neighbor in neighbor_direction_tuple_map:
-        direction = None
-        new_coords = (coord[0] + neighbor_direction_tuple_map[neighbor][0], coord[1] + neighbor_direction_tuple_map[neighbor][1])
-        are_new_coords_out_of_range = new_coords[0] < 0 or new_coords[0] >= len(grid) or new_coords[1] < 0 or new_coords[1] >= len(grid[0])
-        if not are_new_coords_out_of_range and grid[new_coords[0]][new_coords[1]] == 'M':
-            print("Found M")
-            counter += 1
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] == 'A':
+                mas_counter = 0
+                for direction in diag_neighbor_direction_tuple_map:
+                    new_coord = add_direction_to_coord((y, x), direction)
+                    opposite_coord = add_direction_to_coord((y, x), neighbor_compliment[direction])
 
-        # Check to see if neighbor is valid, if it IS, set direction and check to the end. If success, increment counter.
+                    if new_coord[0] < 0 or new_coord[0] >= len(grid) or new_coord[1] < 0 or new_coord[1] >= len(grid[0]):  ## If Out of Range
+                        continue
 
-        print(neighbor)
+                    if opposite_coord[0] < 0 or opposite_coord[0] >= len(grid) or opposite_coord[1] < 0 or opposite_coord[1] >= len(grid[0]):  ## If Out of Range
+                        continue
 
-    # For each "correct" neighbor, check in it's direction until you find 's" or not
+                    if grid[new_coord[0]][new_coord[1]] == "M" and grid[opposite_coord[0]][opposite_coord[1]] == "S":
+                        mas_counter += 1
 
-    # IF you do find "S" break true, else break false
-
-
+                if mas_counter >= 2:
+                    counter += 1
 
     return counter
+
+def depth_first_search_for_christmas(coord, grid):
+    counter = 0
+    for direction in neighbor_direction_tuple_map:
+        if does_direction_contain_full_string(coord, grid, direction, 'M'):
+            counter += 1
+
+    return counter
+
+def add_direction_to_coord(coord, direction):
+    return coord[0] + neighbor_direction_tuple_map[direction][0], coord[1] + neighbor_direction_tuple_map[direction][1]
+
+def does_direction_contain_full_string(coord, grid, direction, goal_letter):
+    next_goal_letter = next_letter_map[goal_letter]
+    new_coord = add_direction_to_coord(coord, direction)
+
+    if new_coord[0] < 0 or new_coord[0] >= len(grid) or new_coord[1] < 0 or new_coord[1] >= len(grid[0]): ## If Out of Range
+        return False
+
+    if grid[new_coord[0]][new_coord[1]] == "S" and goal_letter == "S": # If Found final letter
+        return True
+
+    if grid[new_coord[0]][new_coord[1]] == goal_letter: # If found next letter but not final Letter
+        return does_direction_contain_full_string(new_coord, grid, direction, next_goal_letter)
+    else: # Did not find next letter
+        return False
+
 
 def read_input(filename):
     script_path = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(script_path, filename)
     file = open(filepath, 'r')
-    word_search_grid = file.readlines()
+    raw_grid = file.readlines()
+    sanitized_grid = [line.strip() for line in raw_grid]
 
-    return word_search_grid
+    return sanitized_grid
 
 
 if __name__ == "__main__":
