a593ba07ebb3fc1d9e5ee8793c39e3fdb0a5efde
wcchristian <c.andersen2012@gmail.com>
2024-12-08 16:22:52 -0600

    Day 8

--- 2024/day8/day8.py
+++ 2024/day8/day8.py
@@ -1,3 +1,4 @@
+from math import lcm
 import os
 
 def main():
@@ -5,9 +6,9 @@ def main():
     example_filename = "day8_example.txt"
 
     print(f'Part 1 Example: {part1(example_filename)}')
-    # print(f'Part 1: {part1(filename)}')
-    # print(f'Part 2 Example: {part2(example_filename)}')
-    # print(f'Part 2: {part2(filename)}')
+    print(f'Part 1: {part1(filename)}')
+    print(f'Part 2 Example: {part2(example_filename)}')
+    print(f'Part 2: {part2(filename)}')
     
 
 def part1(filename):
@@ -15,41 +16,77 @@ def part1(filename):
     antinodes = set()
     antenna_dict = dict()
 
-    # Find the coords of all antennas, dictionary of frequency: [points]?
+    # Find all of the antennas
     for y in range(len(grid)):
         for x in range(len(grid[y])):
             if grid[y][x].isalnum():
-                antenna_dict[grid[y][x]] = (y, x)
+                if not grid[y][x] in antenna_dict:
+                    antenna_dict[grid[y][x]] = []
+                antenna_dict[grid[y][x]].append((y, x))
 
+    # Find all antinodes
+    for item in antenna_dict.items():
+        for coord in item[1]:
+            for antenna in item[1]:
+                y_distance = coord[0] - antenna[0]
+                x_distance = coord[1] - antenna[1]
+                antenna_distance = (y_distance, x_distance)
+                if antenna_distance != (0, 0):
+                    antinode_y = coord[0] + y_distance
+                    antinode_x = coord[1] + x_distance
 
-    # For each antenna
-        # for each other antenna at this frequency
-            # where would the point be? #MATH
-            # add this point to our set of possible antinodes.
-
-
-        # find it's compliments in the list of points.
-        # add the antinode to the list of possible antinodes IF it can be located on the grid
-
-    # Loop through the grid
-        # if point is an antinode
-            # Add to set?
-
-
-    # return count of length of the set
+                    # if in bounds
+                    if antinode_y >= 0 and antinode_y <= len(grid) - 1 and antinode_x >= 0 and antinode_x <= len(grid[0]) - 1:
+                        antinodes.add((antinode_y, antinode_x))
 
     return len(antinodes)
 
 
 def part2(filename):
-    lines = read_input(filename)
+    grid = read_input(filename)
+    antinodes = set()
+    antenna_dict = dict()
 
-    return 0
+    # Find all of the antennas
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x].isalnum():
+                if not grid[y][x] in antenna_dict:
+                    antenna_dict[grid[y][x]] = []
+                antenna_dict[grid[y][x]].append((y, x))
 
 
-def is_antinode(coord, grid):
-    # Check in each direction until you fin
-    return
+    # Find all antinodes
+    for item in antenna_dict.items():
+        for coord in item[1]:
+            for antenna in item[1]:
+                y_distance = coord[0] - antenna[0]
+                x_distance = coord[1] - antenna[1]
+                if not y_distance == 0 and not x_distance == 0: # If this is NOT the same space that we started
+                    antinode_y = coord[0]
+                    antinode_x = coord[1]
+
+                    # if in bounds
+                    while antinode_y >= 0 and antinode_y < len(grid) and antinode_x >= 0 and antinode_x < len(grid[0]):
+                        antinodes.add((antinode_y, antinode_x))
+                        antinode_y += y_distance
+                        antinode_x += x_distance
+
+                # opposite direction
+                y_distance = - coord[0] - antenna[0]
+                x_distance = - coord[1] - antenna[1]
+                if not y_distance == 0 and not x_distance == 0: # If this is NOT the same space that we started
+                    antinode_y = coord[0]
+                    antinode_x = coord[1]
+
+                    # if in bounds
+                    while antinode_y >= 0 and antinode_y < len(grid) and antinode_x >= 0 and antinode_x < len(grid[0]):
+                        antinodes.add((antinode_y, antinode_x))
+                        antinode_y += y_distance
+                        antinode_x += x_distance
+
+    # return len([anti for anti in antinodes if anti[0] > 0 and anti[1] > 0])
+    return len(antinodes)
 
 
 def read_input(filename):
