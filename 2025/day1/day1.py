3b0108f400461f07e630193e071e91b73cd697b0
wcchristian <c.andersen2012@gmail.com>
2025-12-01 00:04:59 -0600

    Initial Commit for Day 01 of 2025

---
+++ 2025/day1/day1.py
@@ -0,0 +1,81 @@
+from pathlib import Path
+from math import floor
+
+def main():
+    filename = "day1_input.txt"
+    example_filename = "day1_example.txt"
+
+    print(f'Part 1 Example: {part1(example_filename)}')
+    print(f'Part 1: {part1(filename)}')
+    print(f'Part 2 Example: {part2(example_filename)}')
+    print(f'Part 2: {part2(filename)}')
+    
+
+def part1(filename):
+    lines = read_file(filename)
+    position = 50
+    counter = 0 
+
+    for line in lines:
+        direction = line[0]
+        distance = int(line[1:])
+
+        if direction == "R":
+            position += distance
+        elif direction == "L":
+            position -= distance
+
+        position = position % 100
+
+        if position == 0:
+            counter += 1
+
+    return counter
+
+
+# Feel like I could make this part better.
+def part2(filename):
+    position = 50
+    counter = 0 
+    lines = read_file(filename)
+
+    for line in lines:
+        starting_pos = position
+        direction = line[0]
+        full_distance = int(line[1:])
+        distance = full_distance % 100
+        counter += floor(full_distance / 100)
+
+        if direction == "R" and position + distance > 99:
+            position = (position + distance) - 100
+            counter += 1 if starting_pos != 0 else 0
+        elif direction == "R":
+            position += distance
+            counter += 1 if position == 0 else 0
+        elif direction == "L" and position - distance < 0:
+            position = (100 - abs(position - distance))
+            counter +=  1 if starting_pos != 0 else 0
+        elif direction == "L":
+            position -= distance
+            counter += 1 if position == 0 else 0
+
+    return counter
+
+
+
+
+def read_file(filename):
+    path = Path(__file__).parent / filename
+    with open(path, 'r') as f:
+        data = f.read().strip().splitlines()
+    return data
+
+if __name__ == "__main__":
+    main()
+
+
+#     advent-of-code/2025/day1/day1.py
+# Part 1 Example: 3
+# Part 1: 1139
+# Part 2 Example: 6
+# Part 2: 6684
